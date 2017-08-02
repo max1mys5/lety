0e1ad8fa4666bf9dbf43c81352c7ad943390dcbd
Максим Коломиец < >
2017-08-02 22:57:27 +0300

    m

---
+++ gulpfile.js
@@ -0,0 +1,101 @@
+var gulp = require('gulp'),
+	connect = require('gulp-connect'),
+	sass = require('gulp-sass'),
+	autoprefixer = require('gulp-autoprefixer'),
+	minifyCSS = require('gulp-clean-css'),
+	uglify = require('gulp-uglify'),
+	minify = require('gulp-minify'),
+	concat = require('gulp-concat'),
+	sprite = require('gulp-sprite-generator'),
+	spritesmith = require('gulp.spritesmith');
+
+var startFolder = 'src/',
+	finishFolder = 'dist/';
+
+
+gulp.task('connect', function() {
+	connect.server({
+		root: finishFolder,
+		port: process.env.PORT || 8080,
+		livereload: true,
+		livereloadPort: 3000
+	});
+});
+
+gulp.task('html',function() {
+	gulp.src(startFolder + '/**/**/**.html')
+		.pipe(gulp.dest(finishFolder))
+		.pipe(connect.reload());
+});
+
+gulp.task('fonts',function() {
+	gulp.src(startFolder + '/fonts/**.*')
+		.pipe(gulp.dest(finishFolder + 'fonts'))
+		.pipe(connect.reload());
+});
+
+
+gulp.task('sass', ['bower_styles','images'], function() {
+	return gulp.src(startFolder + 'css/style.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(autoprefixer({
+			browsers: ['last 20 versions'],
+			cascade: false
+		}))
+		.pipe(concat('style.css'))
+		// .pipe(minifyCSS({compatibility: 'ie8'}))
+		.pipe(gulp.dest(finishFolder + 'css/'))
+		.pipe(connect.reload());
+});
+gulp.task('bower_styles', function() {
+	gulp.src([
+
+			])
+		.pipe(concat('bower_components.css'))
+		.pipe(minifyCSS({compatibility: 'ie8'}))
+		.pipe(gulp.dest(finishFolder + 'css/'));
+});
+
+ gulp.task('images', function () {
+   gulp.src(startFolder + 'img/**/*')
+		.pipe(gulp.dest(finishFolder + 'img/'));
+});
+
+gulp.task('sprite', function () {
+  var spriteData = gulp.src('./src/img/icons/*.png').pipe(spritesmith({
+    imgName: 'sprite.png',
+    cssName: 'sprite.css'
+  }));
+  return spriteData.pipe(gulp.dest(finishFolder + 'css/'));
+});
+
+gulp.task('js', function() {
+	gulp.src([
+			startFolder + '**/**/**/**/**.js'
+
+
+		])
+		.pipe(concat('app.js'))
+		// .pipe(minify())
+		// .pipe(uglify())
+		.pipe(gulp.dest(finishFolder + 'js/'))
+		.pipe(connect.reload());
+
+	gulp.src([
+			])
+		.pipe(concat('bower_components.js'))
+		// .pipe(minify())
+		// .pipe(uglify())
+		.pipe(gulp.dest(finishFolder + 'js'));
+});
+
+gulp.task('watch', function() {
+	gulp.watch(startFolder + '**.html', ['html']);
+	gulp.watch(startFolder + '**/**/**/**/**.js', ['js']);
+	gulp.watch(startFolder + 'css/**/**.*', ['sass']);
+});
+
+gulp.task('build', ['html', 'sass', 'js', 'fonts', 'sprite'], function() {
+
+});
+gulp.task('default', ['connect', 'watch', 'build']);
